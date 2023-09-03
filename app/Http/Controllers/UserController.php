4198d7d565cdc5a3e0c956aee10537a0724c3374
mmustafamsm <mustafamsm8@gmail.com>
2023-09-03 20:11:38 +0300

    blade edit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,6 +18,7 @@ class UserController extends Controller
      */
     public function index(Request $request)
     {
+    
         // $users=User::query()
         // ->when($request->search,function($query,$search){
         //     $query->where('name','LIKE',"%{$search}%");
@@ -35,57 +36,10 @@ class UserController extends Controller
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
+    
 
     public function export($ids = null)
     { 
-       
      
         if ($ids) {
             $data = explode(',', $ids);
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,20 +1,58 @@
 <script setup>
+import { ref } from 'vue';
 import AppLayout from '@/Layouts/AppLayout.vue';
 import Welcome from '@/Components/Welcome.vue';
+import DialogModal from '@/Components/DialogModal.vue';
+import SecondaryButton from '@/Components/SecondaryButton.vue';
+import DangerButton from '@/Components/DangerButton.vue';
+
+
+let confirmingUserDeletion = ref(false);
 </script>
 
 <template>
     <AppLayout title="Dashboard">
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Dashboard 
+                Dashboard
             </h2>
         </template>
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <Welcome />
+
+                    <SecondaryButton @click.native="confirmingUserDeletion = true">
+                        open
+                    </SecondaryButton>
+
+                        <DialogModal :show="confirmingUserDeletion" @close="confirmingUserDeletion = false">
+                            <template #title>
+                                Delete Account
+                            </template>
+
+                            <template #content>
+                                Are you sure you want to delete your account? Once your account is deleted, all of its
+                                resources and data will be permanently deleted. Please enter your password to confirm you
+                                would like to permanently delete your account.
+                            </template>
+
+                            <template #footer>
+                                <DangerButton @click="confirmingUserDeletion = false">
+                                    Nevermind
+                                </DangerButton>
+
+                                <form >
+
+
+                                    <DangerButton type="submit" class="ml-2">
+                                        Delete Account
+                                    </DangerButton>
+                                </form>
+                            </template>
+                        </DialogModal>
+
+                        <!-- <Welcome /> -->
                 </div>
             </div>
         </div>
